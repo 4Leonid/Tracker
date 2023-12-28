7382dc8116700d082d2aa67bf2660bf9af4c18a0
Leo <leo-turko@mail.ru>
2023-12-28 18:12:22 +0300

    Add all changes to app

---
+++ Tracker/Helpers/UIStackView+Extention.swift
@@ -0,0 +1,29 @@
+//
+//  UIStackView+Extention.swift
+//  Tracker
+//
+//  Created by Леонид Турко on 28.12.2023.
+//
+
+import UIKit
+
+extension UIStackView {
+    
+    // MARK: - Configure Method for Placeholders
+    func configure(name: String, text: String) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        let image = UIImageView()
+        image.image = UIImage(named: name)
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        label.text = text
+        label.textColor = .yaBlack
+        self.spacing = 8
+        self.axis = .vertical
+        self.alignment = .center
+        self.addArrangedSubview(image)
+        self.addArrangedSubview(label)
+    }
+}
+
+
